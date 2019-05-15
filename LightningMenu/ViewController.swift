f26fef51b66da4245715c7d7861bfd1561398c8e
zaru <zarutofu@gmail.com>
2019-05-15 22:32:46 +0900

    Initial Commit

---
+++ LightningMenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LightningMenu
+//
+//  Created by zaru on 2019/05/15.
+//  Copyright © 2019 zaru. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
